c3309d07f3fc9da02fc9cafbce78fb33ac4377ea
Kasper Mikiewicz <IderedPL@gmail.com>
2013-04-07 15:37:55 +0200

    update

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,35 +1,4 @@
 module.exports = function(grunt) {
-<<<<<<< HEAD
-
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: '_ui/js/script.js',
-        dest: '_ui/js/script.min.js'
-      }
-    },
-    cssmin: {
-      with_banner: {
-        options: {
-          banner: '/* <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */'
-        },
-        files: {
-          '_ui/style.min.css': ['_ui/*.css']
-        }
-      }
-    }
-  });
-
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-
-  grunt.registerTask('default', ['uglify', 'cssmin']);
-=======
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         uglify: {
@@ -75,5 +44,4 @@ module.exports = function(grunt) {
     grunt.registerTask('default', ['jshint', 'uglify', 'cssmin', 'imagemin']);
     grunt.registerTask('min',     ['uglify', 'cssmin', 'imagemin']);
     grunt.registerTask('jsdebug', ['jshint']);
->>>>>>> docs-new
 };
